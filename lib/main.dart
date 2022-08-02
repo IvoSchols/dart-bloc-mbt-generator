f1e68b4538790e1b86391237aea287a8d550f43c
Ivo <ivoschols97@gmail.com>
2022-08-02 12:28:02 +0200

    added test oracle factory

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 // @dart=2.9
-import 'package:dart_bloc_mbt_generator/PathGenerator/PathGenerator.dart';
-import 'package:dart_bloc_mbt_generator/PathGenerator/SimplePathGenerator.dart';
+import 'package:dart_bloc_mbt_generator/path_generator/PathGenerator.dart';
+import 'package:dart_bloc_mbt_generator/path_generator/SimplePathGenerator.dart';
 import 'package:dart_bloc_mbt_generator/examples/cubit_examples/simpleAB/statemachine/simpleAB_statemachine.dart';
 import 'package:state_machine/state_machine.dart';
 
 void main(List<String> args) {
-  final machine = constructSimpleABStatemachine();
+  final StateMachine machine = constructSimpleABStatemachine();
 
   PathGenerator pathGenerator = SimplePathGenerator();
   List<Paths> allPaths = pathGenerator.generateAllPaths(machine);
---
+++ lib/test_oracle/cubit_oracle.dart
@@ -0,0 +1,3 @@
+import 'package:dart_bloc_mbt_generator/test_oracle/test_oracle.dart';
+
+class CubitOracle extends TestOracle {}
